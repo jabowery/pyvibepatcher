8ce08f43b9bbf4cdbffd2d95652de12251821b8a
James Bowery <jabowery@gmail.com>
2025-08-15 19:39:59 -0500

    Clarify modification_description function and emphasize its use.

--- concatdirtofile.sh
+++ concatdirtofile.sh
@@ -3,5 +3,5 @@ rm output.txt
 find $1 -type f -name '*.py'| while read -r file; do                                                                       
    echo "File: $file" >> output.txt                                                                            
    cat "$file" >> output.txt                                                                                   
-   echo -e "\n" >> output.txt                                                                                  
+   echo "\n" >> output.txt                                                                                  
 done
