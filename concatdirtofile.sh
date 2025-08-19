ec5a70d692d83f1cddbb47d6b3479eeb79de592a
James Bowery <jabowery@gmail.com>
2025-08-18 19:15:28 -0500

    belongs in another repo

--- concatdirtofile.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-rm -f output.txt
-
-# Use "$@" to pass all script arguments to the find command
-find "$@" -type f -name '*.py' | while read -r file; do
-    echo "File: $file" >> output.txt
-    cat "$file" >> output.txt
-    echo "\n" >> output.txt
-done
